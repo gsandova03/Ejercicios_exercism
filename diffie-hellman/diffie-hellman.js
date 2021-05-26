2546a929e0eb486101be57aff0efcc174115b4bb
Gabriel Sandoval <gsrivillas0328@gmail.com>
2021-05-26 10:11:01 -0500

    Ejercicio resuelto

---
+++ diffie-hellman/diffie-hellman.js
@@ -0,0 +1,19 @@
+//
+// This is only a SKELETON file for the 'Diffie Hellman' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export class DiffieHellman {
+  constructor(p, g) {
+    this.p = p
+    this.g = g
+  }
+
+  getPublicKey(privateKey) {
+    
+  }
+
+  getSecret(theirPublicKey, myPrivateKey) {
+    throw new Error('Remove this statement and implement this function');
+  }
+}
